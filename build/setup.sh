69d1349e830e8cb4856ae4bda193d7e0302b5357
ajoergensen <31108516+ajoergensen@users.noreply.github.com>
2020-12-04 13:18:47 +0100

    Add sqlite3 module

--- build/setup.sh
+++ build/setup.sh
@@ -18,7 +18,7 @@ apt-get -y install nginx php$PHP_VERSION-fpm php$PHP_VERSION-mysql php$PHP_VERSI
 	php$PHP_VERSION-enchant php$PHP_VERSION-gd php$PHP_VERSION-imap php$PHP_VERSION-intl \
 	php$PHP_VERSION-json php$PHP_VERSION-pspell php$PHP_VERSION-tidy php$PHP_VERSION-xml \
 	php$PHP_VERSION-xmlrpc php-pear php$PHP_VERSION-zip php$PHP_VERSION-bz2 php$PHP_VERSION-mbstring  php$PHP_VERSION-curl \
-	php$PHP_VERSION-pgsql php$PHP_VERSION-ldap
+	php$PHP_VERSION-pgsql php$PHP_VERSION-ldap php$PHP_VERSION-sqlite3
 
 if [[ "$PHP_VERSION" != '7.4' ]]
 then
